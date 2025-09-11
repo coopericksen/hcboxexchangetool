f84eac77358fec3f80d4430bbd75b65886772f7b
Cooper Ericksen <devcoopa@gmail.com>
2025-09-11 10:48:26 -0700

    Fixed unbalanced groups and file reading

--- main.js
+++ main.js
@@ -32,19 +32,80 @@ csvUpload.addEventListener("change", (e) => {
             let person = new Person(row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7], row[8], row[9] - "\r");
         }
 
+        checkGroupSize();
+
         addButtonToContainer(peopleContainer);
         addButtonToContainer(peopleContainer2);
         addButtonToContainer(peopleContainer3);
         addButtonToContainer(peopleContainer4);
+
+        for (let i = 0; i < peopleObjs.length; i++) {
+            // create arrow
+            arrowsContainer.innerHTML += "&rarr;<br>";
+            arrowsContainer2.innerHTML += "&rarr;<br>";
+        }
+
         assignMatches();
     };
     reader.readAsText(csvFile);
 
 });
 
+function checkGroupSize() {
+    let lowerclass = peopleObjs.length;
+    let upperclass = peopleObjs2.length;
+    let difference = Math.abs(upperclass - lowerclass);
+    if (difference % 2 != 0) {
+        console.log("Odd number of participants")
+        return;
+    }
+
+    if (upperclass > lowerclass) {
+        for (let i = 0; i < difference/2; i++) {
+            let obj = peopleObjs2.pop();
+            peopleObjs3.pop();
+            peopleContainer2.removeChild(obj.element);
+            peopleContainer3.removeChild(obj.element2);
+
+            peopleObjs.push(obj);
+            peopleObjs4.push(obj);
+            peopleContainer.appendChild(obj.element);
+            peopleContainer4.appendChild(obj.element2);
+
+            obj.element.classList.remove("person2");
+            obj.element2.classList.remove("person");
+            obj.element.classList.add("person");
+            obj.element2.classList.add("person2");
+
+            obj.element.textContent = `${obj.email}   |   ${obj.grade}th   |   ${obj.fname} ${obj.lname}`;
+            obj.element2.textContent = `${obj.fname} ${obj.lname}   |   ${obj.grade}th   |   ${obj.email}`;
+        }
+    } else if (lowerclass > upperclass) {
+        for (let i = 0; i < difference/2; i++) {
+            let obj = peopleObjs.pop();
+            peopleObjs4.pop();
+            peopleContainer.removeChild(obj.element);
+            peopleContainer4.removeChild(obj.element2);
+
+            peopleObjs2.push(obj);
+            peopleObjs3.push(obj);
+            peopleContainer2.appendChild(obj.element);
+            peopleContainer3.appendChild(obj.element2);
+            obj.element.classList.remove("person");
+            obj.element2.classList.remove("person2");
+            obj.element.classList.add("person2");
+            obj.element2.classList.add("person");
+
+            obj.element.textContent = `${obj.fname} ${obj.lname}   |   ${obj.grade}th   |   ${obj.email}`;
+            obj.element2.textContent = `${obj.email}   |   ${obj.grade}th   |   ${obj.fname} ${obj.lname}`;
+        }
+    }
+}
+
 function parseCSV(text) {
-  const rows = text.trim().split('\n');
-  return rows.map(row => row.split(','));
+    // console.log(text)
+    const rows = text.trim().split('\n');
+    return rows.map(row => row.split('\t'));
 }
 
 class Person {
@@ -94,35 +155,28 @@ class Person {
             this.element.classList.add("person2");
             this.element2.classList.add("person");
 
-            // create arrow
-            arrowsContainer.innerHTML += "&rarr;<br>";
-            arrowsContainer2.innerHTML += "&rarr;<br>";
         }
 
-        // copy email
+        // mail merge functionality
         this.element.addEventListener("click", () => {
             // let copyText = `Hello ${this.fname},\nThank you for choosing to participate in the 2025 Homecoming Box Exchange. You have been designated to create the box for ${this.assigned.fname} ${this.assigned.lname} of the ${this.assigned.grade}th grade.`
             // navigator.clipboard.writeText(copyText);
             // alert(`Copied email for ${this.fname} ${this.lname} to clipboard.`);
-
-            const subject = "Homecoming Box Exchange 2025";
-            const body = `Hello ${this.fname},\n\nThank you for choosing to participate in the 2025 Homecoming Box Exchange.\n\nYou have been designated to create a box for ${this.assigned.fname} ${this.assigned.lname} of the ${this.assigned.grade}th grade.\n\n${this.assigned.fname}'s favorite drinks are: ${this.assigned.drinks}\n${this.assigned.fname}'s favorite snacks & candy are: ${this.assigned.snacks}\n\nThe box exchange will take place on Friday, September 26, 2025. Your goal should be to create a fun, themed box that has a value of at least $25. If you need assistance with financing your box, please email either Mrs. Smith or Mr. Matthews.\n\nPlease put effort into making a box that you would be happy with receiving.\n\nHappy box building and please do enjoy Homecoming Week!\n\nSincerely,\n\nNHS Leadership Team`
-
-            window.open(`https://mail.google.com/mail/?view=cm&to=${this.email}&su=${encodeURIComponent(subject)}&body=${encodeURIComponent(body)}`);
+            emailFor(this);
         })
 
         this.element2.addEventListener("click", () => {
-            // let copyText = `Hello ${this.fname},\nThank you for choosing to participate in the 2025 Homecoming Box Exchange. You have been designated to create the box for ${this.assigned.fname} ${this.assigned.lname} of the ${this.assigned.grade}th grade.`
-            // navigator.clipboard.writeText(copyText);
-            // alert(`Copied email for ${this.fname} ${this.lname} to clipboard.`);
-
-            const subject = "Homecoming Box Exchange 2025";
-            const body = `Hello ${this.fname},\n\nThank you for choosing to participate in the 2025 Homecoming Box Exchange.\n\nYou have been designated to create a box for ${this.assigned.fname} ${this.assigned.lname} of the ${this.assigned.grade}th grade.\n\n${this.assigned.fname}'s favorite drinks are: ${this.assigned.drinks}\n${this.assigned.fname}'s favorite snacks & candy are: ${this.assigned.snacks}\n\nThe box exchange will take place on Friday, September 26, 2025. Your goal should be to create a fun, themed box that has a value of at least $25. If you need assistance with financing your box, please email either Mrs. Smith or Mr. Matthews.\n\nPlease put effort into making a box that you would be happy with receiving.\n\nHappy box building and please do enjoy Homecoming Week!\n\nSincerely,\n\nNHS Leadership Team`
-
-            window.open(`https://mail.google.com/mail/?view=cm&to=${this.email}&su=${encodeURIComponent(subject)}&body=${encodeURIComponent(body)}`);
+            emailFor(this);
         })
 
     }
+}
+
+function emailFor(obj) {
+    const subject = "Homecoming Box Exchange 2025";
+    const body = `Hello ${obj.fname},\n\nThank you for choosing to participate in the 2025 Homecoming Box Exchange.\n\nYou have been designated to create a box for ${obj.assigned.fname} ${obj.assigned.lname} of the ${obj.assigned.grade}th grade.\n\n${obj.assigned.fname}'s favorite drinks are: ${obj.assigned.drinks}\n${obj.assigned.fname}'s favorite snacks & candy are: ${obj.assigned.snacks}\n\nThe box exchange will take place on Friday, September 26, 2025. Your goal should be to create a fun, themed box that has a value of at least $25. If you need assistance with financing your box, please email either Mrs. Smith or Mr. Matthews.\n\nPlease put effort into making a box that you would be happy with receiving.\n\nHappy box building and please do enjoy Homecoming Week!\n\nSincerely,\n\nNHS Leadership Team`
+
+    window.open(`https://mail.google.com/mail/?view=cm&to=${obj.email}&su=${encodeURIComponent(subject)}&body=${encodeURIComponent(body)}`);
 }
 
 function shuffleArray(array) {
